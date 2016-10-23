9dcab91acd80515a8bcaf83b2dff7f2646022ae8
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-23 18:26:40 +0200

    modif css normal, resultat et erreur ok, reste le bouton restart

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ console.log('hello world');
 		mois : null,
 		annee: null,
 		mydate: null,
-		resultday: null,
+		resultDay: null,
 
 			
 		init : function(){
 			moment.locale('fr')
 			this.listners();
-			$('#message').hide();
+			$('#message1').hide();
 			$('#message2').hide();
 			$('#resultat').hide();
 		},
@@ -22,46 +22,64 @@ console.log('hello world');
 		recupInput : function(){
 			// day.error();
 			day.jour = parseInt($("#jour").val(), 10);
+			$("#jour").css("color", "#47b8e0");
 			console.log(day.jour);
 			day.mois = $('#selectmois').val();
+			$("#selectmois").css("color", "#47b8e0");
 			console.log(day.mois);
 			day.annee = parseInt($('#annee').val(), 10);
+			$("#annee").css("color", "#47b8e0");
 			console.log(day.annee);
 			day.myDate = moment().year(day.annee).month(day.mois).date(day.jour);
+			day.resultDay = day.myDate.format('dddd');
 			day.verifJour();
 			day.verifAnnee();
-			day.resultDay = day.myDate.format('dddd');
+			// day.result();
 			console.log(day.resultDay);
-			day.result();
 
 		},
 
+		// result : function(){
+		// 	$("#resultat").show();
+		// 	$("#resultat").text(day.resultDay);
+		// 	$("#jourmoisannee").hide();
+		// 	$('#restart').click(function(){
+		// 		$("#jourmoisannee").show();
+
+		// 	});
+
+		// },
 		verifJour: function(){
 			if(day.jour < 1 || day.jour > 31){
 				console.log(day.jour);
-				$('#message').show();
+				$('#message1').show();
 				$('#jour').css("border", "2px solid red");
+				day.resultDay= null;
+			}else{
+				$("#resultat").show();
+				$("#resultat").text(day.resultDay);
+				$('#message1').hide();
+				$("#jourmoisannee").hide();
+				day.resultDay= null;
+				$('#restart').click(function(){
+					$("#jourmoisannee").show();
+
+				});
 			}
 		},
+
 		verifAnnee :function(){
-			if(day.annee < 0){
+			if(day.annee <= 0){
 				$('#message2').show();
+				console.log('coucou');
+				$('#resultat').hide();
+				$("#jourmoisannee").show();
 			}
 		},
-		result : function(){
-			$("#resultat").show();
-			$("#resultat").text(day.resultDay);
-			$("#jourmoisannee").hide();
-			$('#restart').click(function(){
-				$("#jourmoisannee").show();
-
-			});
-
-		},
-			
 
 	};
 
 
 day.init();
+
 })();
